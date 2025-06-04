4b82b8b4c0dee805cacee93e317dc65423e5295a
Behnam Asaei <behnamasaei@gmail.com>
2025-06-04 19:32:35 +0330

    add mokeb dtos

---
+++ MokebManagerV2/src/MokebManagerV2.Application/Dtos/CreateUpdateMokebDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MokebManagerV2.Models;
+
+namespace MokebManagerV2.Dtos
+{
+    public class CreateUpdateMokebDto
+    {
+        [Required,MaxLength(300, ErrorMessage = "نام موکب باید حداکثر 300 کاراکتر باشد.")]
+        public string Name { get; set; }
+
+        [Required, Range(0, int.MaxValue, ErrorMessage = "ظرفیت باید عددی مثبت باشد.")]
+        public int Capacity { get; set; }
+
+        [MaxLength(300)]
+        public string Location { get; set; }
+
+        [Required]
+        public Sex Sex { get; set; }
+
+        public Guid? TenantId { get; set; }
+    }
+}
--- MokebManagerV2/src/MokebManagerV2.Application/Dtos/MokebDto.cs
+++ MokebManagerV2/src/MokebManagerV2.Application/Dtos/MokebDto.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using MokebManagerV2.Models;
+using Volo.Abp.Application.Dtos;
 
 namespace MokebManagerV2.Dtos
 {
-    internal class MokebDto
+    public class MokebDto : AuditedEntityDto<Guid>
     {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public string Location { get; set; }
+        public Sex Sex { get; set; }
+        public ICollection<Pilgrim> Pilgrims{ get; set; }
     }
 }

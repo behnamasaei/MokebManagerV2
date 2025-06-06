4953dad168e2dca0fdf46264b6a99eab8e77488f
Behnam Asaei <behnamasaei@gmail.com>
2025-06-06 16:50:39 +0330

    implement Pilgrim appservice

---
+++ MokebManagerV2/src/MokebManagerV2.Application/Services/PilgrimAppService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MokebManagerV2.Dtos;
+using MokebManagerV2.Interfaces;
+using MokebManagerV2.Models;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace MokebManagerV2.Services
+{
+    public class PilgrimAppService : CrudAppService
+        <Pilgrim, PilgrimDto, Guid, PagedAndSortedResultRequestDto, CreateUpdatePilgrimDto, CreateUpdatePilgrimDto>, IPilgrimAppService
+    {
+        public PilgrimAppService(IRepository<Pilgrim, Guid> repository) : base(repository)
+        {
+        }
+    }
+}

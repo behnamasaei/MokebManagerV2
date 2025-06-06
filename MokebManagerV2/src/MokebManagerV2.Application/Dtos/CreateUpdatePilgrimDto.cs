8f1a2cedcf146fafad7ace70c618b105ca74f691
Behnam Asaei <behnamasaei@gmail.com>
2025-06-06 15:49:53 +0330

    add dtos pilgrim

--- MokebManagerV2/src/MokebManagerV2.Application/Dtos/CreateUpdatePilgrimDto.cs
+++ MokebManagerV2/src/MokebManagerV2.Application/Dtos/CreateUpdatePilgrimDto.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using Volo.Abp.Domain.Entities.Auditing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MokebManagerV2.Models;
 using Volo.Abp.MultiTenancy;
+using Volo.Abp.ObjectExtending;
 
-namespace MokebManagerV2.Models
+namespace MokebManagerV2.Dtos
 {
-    public class Pilgrim : FullAuditedAggregateRoot<Guid>, IMultiTenant
+    [Serializable]
+    public class CreateUpdatePilgrimDto : ExtensibleObject, IMultiTenant
     {
-        [MaxLength(100 , ErrorMessage = "حداکثر 100 کاراکتر باشد.")]
+        [MaxLength(100, ErrorMessage = "حداکثر 100 کاراکتر باشد.")]
         public string FullName { get; set; }
-        
+
         [MaxLength(15)]
         [Phone(ErrorMessage = "موبایل به درستی وارد شود.")]
         public string PhoneNumber { get; set; }
@@ -18,7 +23,7 @@ namespace MokebManagerV2.Models
         [MaxLength(20)]
         public string NationalCode { get; set; }
 
-        [Required (ErrorMessage = "شماره پاسپورت اجباری می باشد.")]
+        [Required(ErrorMessage = "شماره پاسپورت اجباری می باشد.")]
         [MaxLength(30)]
         public string PassportNo { get; set; }
 
@@ -29,13 +34,14 @@ namespace MokebManagerV2.Models
 
         [Required(ErrorMessage = "موکب انتخاب شود.")]
         public Guid MokebId { get; set; }
-        public virtual Mokeb Mokeb { get; set; }
 
         public string Image { get; set; }
+
         public DateTime EntryDate { get; set; }
+
         public DateTime ForceExitDate { get; set; }
+
         public DateTime ExitDate { get; set; }
-        public virtual List<DateTime> Traffic { get; set; }
 
         public Guid? TenantId { get; set; }
     }
--- MokebManagerV2/src/MokebManagerV2.Application/Dtos/PilgrimDto.cs
+++ MokebManagerV2/src/MokebManagerV2.Application/Dtos/PilgrimDto.cs
@@ -1,40 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MokebManagerV2.Models;
+using Volo.Abp.Application.Dtos;
 using Volo.Abp.Domain.Entities.Auditing;
 using Volo.Abp.MultiTenancy;
 
-namespace MokebManagerV2.Models
+namespace MokebManagerV2.Dtos
 {
-    public class Pilgrim : FullAuditedAggregateRoot<Guid>, IMultiTenant
+    public class PilgrimDto : ExtensibleFullAuditedEntityDto<Guid>, IMultiTenant
     {
-        [MaxLength(100 , ErrorMessage = "حداکثر 100 کاراکتر باشد.")]
         public string FullName { get; set; }
-        
-        [MaxLength(15)]
-        [Phone(ErrorMessage = "موبایل به درستی وارد شود.")]
+
         public string PhoneNumber { get; set; }
 
-        [MaxLength(20)]
         public string NationalCode { get; set; }
 
-        [Required (ErrorMessage = "شماره پاسپورت اجباری می باشد.")]
-        [MaxLength(30)]
         public string PassportNo { get; set; }
 
         public Sex Sex { get; set; }
 
-        [Range(0, int.MaxValue, ErrorMessage = "شماره تخت باید عددی مثبت باشد.")]
         public int BedNumber { get; set; }
 
-        [Required(ErrorMessage = "موکب انتخاب شود.")]
         public Guid MokebId { get; set; }
+
         public virtual Mokeb Mokeb { get; set; }
 
         public string Image { get; set; }
+
         public DateTime EntryDate { get; set; }
+
         public DateTime ForceExitDate { get; set; }
+
         public DateTime ExitDate { get; set; }
+
         public virtual List<DateTime> Traffic { get; set; }
 
         public Guid? TenantId { get; set; }

03d8d93d52cd3c4e0195a2c6ec6a97ff60e46174
Behnam Asaei <behnamasaei@gmail.com>
2025-06-08 17:42:04 +0330

    add duration calclution and qrcode reader

--- MokebManagerV2/src/MokebManagerV2.Web/abp.resourcemapping.js
+++ MokebManagerV2/src/MokebManagerV2.Web/abp.resourcemapping.js
@@ -7,5 +7,7 @@ module.exports = {
     ],
     mappings: {
         "@node_modules/@majidh1/jalalidatepicker": "@libs/majidh1/jalalidatepicker",
+        "node_modules/html5-qrcode/html5-qrcode.min.js": "@libs/html5-qrcode/html5-qrcode.min.js",
+        "/node_modules/jalaali-js/dist/jalaali.min.js": "@libs/jalaali-js"
     }
 };
